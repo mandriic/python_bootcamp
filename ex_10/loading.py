797fde8e01f99768ec8e4bda90e316778dc76441
Sirius <sirius@42madrid.com>
2023-03-14 13:45:00 +0100

    ex_10 in work

--- ex_10/loading.py
+++ ex_10/loading.py
@@ -1,18 +1,37 @@
-import time
-def progress_bar(lst):
+import time as t
+import sys
+def ft_progress(listy):
+    print(listy)
     print("Loading")
-    for elem in lst:
+    kata = ">"
+    cerr = "]"    
+    time_s = t.time()
+    i = 1
+    for elem in listy:
+        time_n = t.time()
+        time_c = time_n - time_s
+        if listy[-1]/42 * i <= elem:
+            i += 1
+        string = "[" + kata.rjust(i, '=') + cerr.rjust(42 - i + 2, " ")
+        sys.stdout.write("ETA: {:.2f}s [{:.0f}%] {s}\r".format(time_c, elem/listy[-1] * 100, s=string))
+        
+        sys.stdout.flush()
+        # sys.stdout.write("ETA: %d%% %d  \r" % (elem) % (time_s)  )
+        # sys.stdout.flush()
         yield elem
-        # print(".", end="", flush=True)
-    
-    
-    
     
 listy = range(1000)
 ret = 0
-for elem in progress_bar(listy): 
-    print(elem, end=" ", flush=True)
-    ret += elem 
-    time.sleep(0.01)
+for elem in ft_progress(listy): 
+    ret+=(elem+3) %5
+    t.sleep(0.01)
 print()
 print(ret)
+
+# # Put this at the top of your kata03.py file
+# import time as t
+# kata = ">"
+# cerr = "]"
+# for i in range(42):
+#     print("[", kata.rjust(i, '='), cerr.rjust(42 - i, " "), flush=True)
+#     t.sleep(1)
--- ex_10/test.py
+++ ex_10/test.py
@@ -5,4 +5,4 @@ for i in range(100):
     progress = i + 1
     sys.stdout.write("Download progress: %d%%   \r" % (progress) )
     sys.stdout.flush()
-    time.sleep(0.1)    
+    time.sleep(0.1) 
